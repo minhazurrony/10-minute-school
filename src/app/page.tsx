a859973451dd6339e01e220bee0d0b19d8106d5c
Minhazur Rahman <minhazur.rony@gmail.com>
2024-12-05 16:27:01 +0600

    refactor: update imports paths with import alias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { getLiveBatch, getVariants } from "@/app/services";
+
+export default async function Home() {
+  const liveBatchData = await getLiveBatch();
+  const variantsData = await getVariants();
+
   return (
     <div>
       <h1>10 minute school</h1>
