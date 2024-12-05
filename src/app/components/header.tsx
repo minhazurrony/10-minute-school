53c381620c1bba4262e092a545a0fd38381025c1
Minhazur Rahman <minhazur.rony@gmail.com>
2024-12-05 19:26:25 +0600

    feat: add header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <nav className="sticky top-0 z-10 flex min-h-16 items-center justify-between border-b">
+      <div className="m-auto flex w-full max-w-[1440px] items-center justify-between px-4 py-3 md:px-7">
+        <Link href={"/"}>
+          <Image
+            src="https://cdn.10minuteschool.com/images/svg/10mslogo-svg.svg"
+            height={27}
+            width={100}
+            alt="10-min-school-logo"
+          />
+        </Link>
+
+        {/* TODO: add localization switch button */}
+      </div>
+    </nav>
+  );
+};
