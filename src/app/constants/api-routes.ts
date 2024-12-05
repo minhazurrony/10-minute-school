90fe12a901b2c6d5ad92087ef55a869b81dbb5f3
Minhazur Rahman <minhazur.rony@gmail.com>
2024-12-05 16:11:50 +0600

    chore: add api endpoints

---
+++ src/app/constants/api-routes.ts
@@ -0,0 +1,4 @@
+export const LIVE_BATCH_ENDPOINT =
+  "https://api.10minuteschool.com/discovery-service/api/v1/products/ielts-live-batch";
+export const VARIANTS_ENDPOINT =
+  "https://api.10minuteschool.com/discovery-service/api/v1/products/ielts-live-batch/variants";
